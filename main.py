f365cbe5827bc7b0c84f513c7ea9c275a96fe7e6
Derek Wales <derek.l.wales@gmail.com>
2021-02-09 00:45:27 +0000

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Derek is done with project one'
 
 
 if __name__ == '__main__':

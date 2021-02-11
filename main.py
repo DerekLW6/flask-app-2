454563f5ab9d423f2e9559d887d29e06878badf4
DerekLW6 <53124417+DerekLW6@users.noreply.github.com>
2021-02-10 20:49:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Sarah is a SMOKE!!!!'
+    return 'Project One Complete!!!!'
 
 
 if __name__ == '__main__':

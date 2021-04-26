5674a3c007aba0272c196231b8334066888b3bdd
DerekLW6 <53124417+DerekLW6@users.noreply.github.com>
2021-04-26 14:57:03 -0400

    Update main.py
    Updated app header

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'I wove you Sarah!'
+    return 'I like Data Science!!'
 
 
 if __name__ == '__main__':
